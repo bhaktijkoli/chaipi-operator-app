7fc1f743a578818c513f5eab232679929f988b3d
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-05-25 17:39:30 +0530

    Partially removed android background message

--- index.js
+++ index.js
@@ -6,8 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import bgMessaging from './app/utils/bgMessaging';
-
 AppRegistry.registerComponent(appName, () => App);
-
-AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging);
